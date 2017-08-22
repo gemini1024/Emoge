6c6e1aa0ae04cc2c450e8be05b47a63460ab6a8a
JiHun-Choi <gemini10244@gmail.com>
2017-08-22 11:19:04 +0900

    add BetterVideoView

--- app/src/main/java/com/emoge/app/emoge/ui/frame/FrameAdder.java
+++ app/src/main/java/com/emoge/app/emoge/ui/frame/FrameAdder.java
@@ -64,18 +64,18 @@ public class FrameAdder implements OnBMClickListener, BitmapLoadable {
                 intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[] {"image/jpeg", "image/png"});
                 intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-                activity.startActivityForResult(Intent.createChooser(intent,activity.getString(R.string.select_image)), INTENT_GET_IMAGE);
+                activity.startActivityForResult(Intent.createChooser(intent,activity.getString(R.string.add_from_image)), INTENT_GET_IMAGE);
                 activity.overridePendingTransition(0, android.R.anim.fade_in);
                 break;
             case INTENT_GET_GIF :
                 intent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 intent.putExtra(Intent.EXTRA_MIME_TYPES, new String[] {"image/gif"});
-                activity.startActivityForResult(Intent.createChooser(intent,activity.getString(R.string.select_image)), INTENT_GET_GIF);
+                activity.startActivityForResult(Intent.createChooser(intent,activity.getString(R.string.add_from_gif)), INTENT_GET_GIF);
                 activity.overridePendingTransition(0, android.R.anim.fade_in);
                 break;
             case INTENT_GET_VIDEO :
                 intent = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
-                activity.startActivityForResult(Intent.createChooser(intent,activity.getString(R.string.select_video)), INTENT_GET_VIDEO);
+                activity.startActivityForResult(Intent.createChooser(intent,activity.getString(R.string.add_from_video)), INTENT_GET_VIDEO);
                 activity.overridePendingTransition(0, android.R.anim.fade_in);
                 break;
         }
--- app/src/main/java/com/emoge/app/emoge/ui/frame/VideoActivity.java
+++ app/src/main/java/com/emoge/app/emoge/ui/frame/VideoActivity.java
@@ -2,19 +2,19 @@ package com.emoge.app.emoge.ui.frame;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
-import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.emoge.app.emoge.R;
 import com.emoge.app.emoge.utils.Logger;
 import com.emoge.app.emoge.utils.SeekBarNumberTransformers;
 import com.emoge.app.emoge.utils.dialog.SweetDialogs;
+import com.halilibo.bettervideoplayer.BetterVideoCallback;
+import com.halilibo.bettervideoplayer.BetterVideoPlayer;
 
 import org.adw.library.widgets.discreteseekbar.DiscreteSeekBar;
 
@@ -28,7 +28,7 @@ import butterknife.OnClick;
  * 정보(캡처 시작 위치, 캡처 수, fps) 획득
  */
 
-public class VideoActivity extends AppCompatActivity {
+public class VideoActivity extends AppCompatActivity implements BetterVideoCallback {
     private static final String LOG_TAG = VideoView.class.getSimpleName();
 
     public static final String INTENT_NAME_START_SEC        = "startSec";
@@ -37,7 +37,7 @@ public class VideoActivity extends AppCompatActivity {
 
 
     @BindView(R.id.video_video)
-    VideoView mVideoView;
+    BetterVideoPlayer mVideoView;
     @BindView(R.id.video_count)
     DiscreteSeekBar mCountBar;
     @BindView(R.id.video_fps)
@@ -59,23 +59,13 @@ public class VideoActivity extends AppCompatActivity {
             }
         });
 
-        mVideoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
-            @Override
-            public boolean onError(MediaPlayer mp, int what, int extra) {
-                Logger.e(LOG_TAG, getString(R.string.err_not_found_video_title));
-                SweetDialogs.showErrorDialog(VideoActivity.this,
-                        R.string.err_not_found_video_title, R.string.err_not_found_video_text);
-                finish();
-                return false;
-            }
-        });
-
         if( getIntent() != null && getIntent().getData() != null ) {
             videoUri = getIntent().getData();
             Logger.i(LOG_TAG, videoUri.toString());
-            mVideoView.setVideoURI(videoUri);
-            mVideoView.setMediaController(new MediaController(mVideoView.getContext()));
-            mVideoView.start();
+
+            mVideoView.setCallback(this);
+            mVideoView.setSource(videoUri);
+            mVideoView.enableSwipeGestures();
 
             mFpsBar.setNumericTransformer(SeekBarNumberTransformers.Multiply(100));
             mFpsBar.setMin(1);
@@ -100,4 +90,49 @@ public class VideoActivity extends AppCompatActivity {
         finish();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mVideoView.pause();
+    }
+
+    @Override
+    public void onStarted(BetterVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onPaused(BetterVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onPreparing(BetterVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onPrepared(BetterVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onBuffering(int percent) {
+
+    }
+
+    @Override
+    public void onError(BetterVideoPlayer player, Exception e) {
+
+    }
+
+    @Override
+    public void onCompletion(BetterVideoPlayer player) {
+
+    }
+
+    @Override
+    public void onToggleControls(BetterVideoPlayer player, boolean isShowing) {
+
+    }
 }
